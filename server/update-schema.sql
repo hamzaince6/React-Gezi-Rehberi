a51963bb017140fa10416f3a046ced8d7479f18c
Hamza İnce <hamzaince001@gmail.com>
2024-12-15 17:10:20 +0300

    tes-2

--- server/update-schema.sql
+++
@@ -1,7 +0,0 @@
--- readTime sütununu nvarchar(50) olarak güncelle
-IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS 
-    WHERE TABLE_NAME = 'Stories' AND COLUMN_NAME = 'readTime')
-BEGIN
-    ALTER TABLE Stories
-    ALTER COLUMN readTime nvarchar(50)
-END
